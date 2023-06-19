cb479eb0a42e9e6f47f2d5c99cca925f8c51600a
eduardocruzpalacios <52535457+eduardocruzpalacios@users.noreply.github.com>
2023-06-19 19:08:21 +0200

    Add BridgeSquare class

--- src/game/Board.java
+++ src/game/Board.java
@@ -1,6 +1,7 @@
 package game;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
@@ -12,6 +13,7 @@ public class Board {
 	private Map<Integer, Square> squares;
 	private final List<Integer> gooseSquaresIds = new ArrayList<>(
 			List.of(1, 5, 9, 14, 18, 23, 27, 32, 36, 41, 45, 50, 54, 59, 63));
+	private final Map<Integer, Integer>bridgeSquaresIdsMap = new HashMap<Integer, Integer>(Map.of(6, 12, 12, 6));
 
 	public Board(GooseGameLogic gooseGameLogic) {
 		this.gooseGameLogic = gooseGameLogic;
@@ -27,6 +29,12 @@ public class Board {
 	public void executeLandedOnGooseSquare(Player player, int currentGooseSquareId) {
 		int nextGooseSquareId = gooseSquaresIds.indexOf(currentGooseSquareId) + 1;
 		player.setSquareId(nextGooseSquareId);
+		this.gooseGameLogic.givePlayerExtraTurn(player);
+	}
+
+	public void executeLandedOnBridgeSquare(Player player, int currentBridgeSquareId) {
+		int theOtherBridgeSquareId  = this.bridgeSquaresIdsMap.get(currentBridgeSquareId);
+		player.setSquareId(theOtherBridgeSquareId);
 		this.gooseGameLogic.givePlayerExtraTurn(player);
 	}
 
---
+++ src/game/squares/BridgeSquare.java
@@ -0,0 +1,18 @@
+package game.squares;
+
+import game.Board;
+import game.Player;
+
+public class BridgeSquare extends Square{
+
+	public BridgeSquare(Integer id, Board board) {
+		super(id, board);
+	}
+
+	@Override
+	public void landedOn(Player player) {
+		this.getBoard().executeLandedOnBridgeSquare(player, this.getId());
+		return;
+	}
+
+}
