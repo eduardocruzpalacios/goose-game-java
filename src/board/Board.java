455c4cdeaf87722a2b21449cded7e655d807fac3
eduardocruzpalacios <52535457+eduardocruzpalacios@users.noreply.github.com>
2023-06-25 16:20:52 +0200

    Refactor to move partial logic of GooseGameLogic class to Board class for being more cohesive

---
+++ src/board/Board.java
@@ -0,0 +1,77 @@
+package board;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import game.GooseGameLogic;
+import player.Player;
+import player.PlayerNoTurnsState;
+import squares.Square;
+
+public class Board {
+
+	private GooseGameLogic gooseGameLogic;
+	private Map<Integer, Player> players;
+	private int playerIdToPlayNextTurn;
+	private Map<Integer, Square> squares;
+	private final List<Integer> gooseSquaresIds = new ArrayList<>(
+			List.of(1, 5, 9, 14, 18, 23, 27, 32, 36, 41, 45, 50, 54, 59, 63));
+	private final Map<Integer, Integer> bridgeSquaresIdsMap = new HashMap<Integer, Integer>(Map.of(6, 12, 12, 6));
+
+	public Board(GooseGameLogic gooseGameLogic, int playersNumber) {
+		this.gooseGameLogic = gooseGameLogic;
+		this.players = new HashMap<Integer, Player>();
+		for (int i = 1; i <= playersNumber; i++) {
+			Player player = new Player(i, this);
+			this.players.put(player.getId(), player);
+		}
+		this.playerIdToPlayNextTurn = 1;
+	}
+
+	public void playerPlaysTurn(Player player, int squareId) {
+		this.makePlayerGoTo(player, squareId);
+		player.setSquareId(squareId);
+		this.giveTurnToNextPlayer();
+		Square square = this.squares.get(squareId);
+		square.landedOn(player);
+	}
+
+	public void makePlayerGoTo(Player player, int squareId) {
+		player.setSquareId(squareId);
+	}
+
+	public void giveTurnToNextPlayer() {
+		if (this.playerIdToPlayNextTurn > this.players.size()) {
+			this.playerIdToPlayNextTurn = 1;
+		} else {
+			this.playerIdToPlayNextTurn++;
+		}
+	}
+
+	public void movePlayerToNextGooseSquare(Player player, int currentGooseSquareId) {
+		int nextGooseSquareId = this.gooseSquaresIds.indexOf(currentGooseSquareId) + 1;
+		player.setSquareId(nextGooseSquareId);
+		this.givePlayerExtraTurn(player);
+	}
+
+	public void givePlayerExtraTurn(Player player) {
+		this.playerIdToPlayNextTurn = player.getId();
+	}
+
+	public void movePlayerToTheOtherBridgeSquare(Player player, int currentBridgeSquareId) {
+		int theOtherBridgeSquareId = this.bridgeSquaresIdsMap.get(currentBridgeSquareId);
+		player.setSquareId(theOtherBridgeSquareId);
+		this.givePlayerExtraTurn(player);
+	}
+
+	public void makePlayerLooseTurns(Player player, int turnsNumber) {
+		player.setPlayerState(new PlayerNoTurnsState(player, turnsNumber));
+	}
+
+	public void finishGame(Player player) {
+		this.gooseGameLogic.finishGame(player);
+	}
+
+}
--- src/game/GooseGameLogic.java
+++ src/game/GooseGameLogic.java
@@ -1,48 +1,20 @@
 package game;
 
-import java.util.HashMap;
-import java.util.Map;
-
 import board.Board;
 import player.Player;
-import player.PlayerNoTurnsState;
 
 public class GooseGameLogic {
 
 	private Board board;
-	private Map<Integer, Player> players;
-	private int playerIdToPlayNextTurn;
 	private GooseGameLogicState gooseGameLogicState;
 
 	public GooseGameLogic(int playersNumber) {
 		super();
-		this.board = new Board(this);
-		this.players = new HashMap<Integer, Player>();
-		for (int i = 1; i <= playersNumber; i++) {
-			Player player = new Player(i, board);
-			this.players.put(player.getId(), player);
-		}
-		this.playerIdToPlayNextTurn = 1;
+		this.board = new Board(this, playersNumber);
 	}
 
 	public void setGooseGameLogicState(GooseGameLogicState gooseGameLogicState) {
 		this.gooseGameLogicState = gooseGameLogicState;
-	}
-
-	public void giveTurnToNextPlayer() {
-		if (this.playerIdToPlayNextTurn > this.players.size()) {
-			this.playerIdToPlayNextTurn = 1;
-		} else {
-			this.playerIdToPlayNextTurn++;
-		}
-	}
-
-	public void givePlayerExtraTurn(Player player) {
-		this.playerIdToPlayNextTurn = player.getId();
-	}
-
-	public void makePlayerLooseTurns(Player player, int turnsNumber) {
-		player.setPlayerState(new PlayerNoTurnsState(player, turnsNumber));
 	}
 
 	public void finishGame(Player player) {
