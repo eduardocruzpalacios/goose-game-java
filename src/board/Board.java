1e7e2cf6f4806221616e268c00d0003d30193553
eduardocruzpalacios <52535457+eduardocruzpalacios@users.noreply.github.com>
2023-07-05 08:08:59 +0200

    Add create squares functionality

--- src/board/Board.java
+++ src/board/Board.java
@@ -9,6 +9,15 @@ import dice.DiceServiceSingleton;
 import game.GooseGameLogicFacade;
 import player.Player;
 import player.PlayerStateHandler;
+import squares.BridgeSquare;
+import squares.BronzeSquare;
+import squares.DiceSquare;
+import squares.GooseSquare;
+import squares.InnSquare;
+import squares.JailSquare;
+import squares.LabyrinthSquare;
+import squares.RegularSquare;
+import squares.SkullSquare;
 import squares.Square;
 
 public class Board {
@@ -33,11 +42,43 @@ public class Board {
 			this.players.put(player.getId(), player);
 		}
 		this.playerIdToPlayNextTurn = 1;
+		this.createSquares();
 		BoardStateHandler.setGameRunningState(this);
 	}
 
 	public void setBoardState(BoardState boardState) {
 		this.boardState = boardState;
+	}
+
+	private void createSquares() {
+		for (Integer gooseSquareId : this.gooseSquaresIds) {
+			Square gooseSquare = new GooseSquare(gooseSquareId, this);
+			this.squares.put(gooseSquareId, gooseSquare);
+		}
+		Square bridgeSquare1 = new BridgeSquare(6, this);
+		this.squares.put(bridgeSquare1.getId(), bridgeSquare1);
+		Square bridgeSquare2 = new BridgeSquare(12, this);
+		this.squares.put(bridgeSquare2.getId(), bridgeSquare2);
+		Square innSquare = new InnSquare(19, this);
+		this.squares.put(innSquare.getId(), innSquare);
+		Square diceSquare1 = new DiceSquare(26, this);
+		this.squares.put(diceSquare1.getId(), diceSquare1);
+		Square diceSquare2 = new DiceSquare(53, this);
+		this.squares.put(diceSquare2.getId(), diceSquare2);
+		Square labyrinthSquare = new LabyrinthSquare(30, this);
+		this.squares.put(labyrinthSquare.getId(), labyrinthSquare);
+		Square bronzeSquare = new BronzeSquare(31, this);
+		this.squares.put(bronzeSquare.getId(), bronzeSquare);
+		Square jailSquare = new JailSquare(52, this);
+		this.squares.put(jailSquare.getId(), jailSquare);
+		Square skullSquare = new SkullSquare(58, this);
+		this.squares.put(skullSquare.getId(), skullSquare);
+		for (int i = 0; i <= 63; i++) {
+			if (this.squares.get(i) == null) {
+				Square regulSquare = new RegularSquare(i, this);
+				this.squares.put(i, regulSquare);
+			}
+		}
 	}
 
 	public void playerPlaysTurnAndLandsOnSquare(Player player, int squareId) {
